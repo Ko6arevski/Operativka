e0ce61f264c433eff6366cc54de01a005a96eb30
Ko6arevski <hr.p.hristov@gmail.com>
2024-11-21 19:49:54 +0200

    add the CLI connection for the RDS and fix the ignore

---
+++ aws_setup/connect_to_rds.sh
@@ -0,0 +1,4 @@
+# RDS is database-1.cfs2cmg40p0q.eu-central-1.rds.amazonaws.com Not sure if the adress changes like the IP address of the EC2 instance.
+# I dont think it should
+
+psql -h database-1.cfs2cmg40p0q.eu-central-1.rds.amazonaws.com -p 5432 -U postgres -d operativka
